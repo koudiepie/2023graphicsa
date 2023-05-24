10e00ee1447f50f871af093db9d4a1998d987e6d
koudiepie <Onityan0804@gmail.com>
2023-05-24 12:27:30 +0800

    week15

--- Final_project/main.cpp
+++ Final_project/main.cpp
@@ -7,11 +7,28 @@ GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
 int show[4] = {0, 1, 1, 1};
 int ID=3;//0頭1身2上手臂3下手臂
+FILE * fout = NULL;
+FILE * fin = NULL;
+float teapotX=0, teapotY=0;
+float angle[20]={};
 void keyboard(unsigned char key, int x, int y){
     if(key=='0') ID=0;
     if(key=='1') ID=1;
     if(key=='2') ID=2;
     if(key=='3') ID=3;
+    if(key=='s'){
+        if(fout==NULL) fout = fopen("motion.txt", "w");
+        for(int i=0;i<20;i++){
+            fprintf(fout, "%.2f ", angle[i] );
+        }
+        fprintf(fout, "\n");
+    }else if(key=='r'){
+        if(fin==NULL) fin = fopen("motion.txt", "r");
+        for(int i=0;i<20;i++){
+            fscanf(fin, "%f", &angle[i] );
+        }
+        glutPostRedisplay();
+    }
     ///if(key=='0') show[0] = !show[0];
     ///if(key=='1') show[1] = !show[1];
     ///if(key=='2') show[2] = !show[2];
@@ -19,10 +36,6 @@ void keyboard(unsigned char key, int x, int y){
     glutPostRedisplay();
 }
 
-FILE * fout = NULL;
-FILE * fin = NULL;
-float teapotX=0, teapotY=0;
-float angle=0, angle2=0, angle3=0;
 void display()
 {
 
@@ -47,7 +60,7 @@ void display()
         if(show[1]) glmDraw(body, GLM_MATERIAL);
         glPushMatrix();
             glTranslatef(-1.186666, +0.380000, 0);
-            glRotatef(angle, 0, 0, 1);
+            glRotatef(angle[2], 0, 0, 1);
             glTranslatef(1.186666, -0.380000, 0);
             if(ID==2) glColor3f(1,0,0);
             else glColor3f(1,1,1);
@@ -56,7 +69,7 @@ void display()
             glPushMatrix();
                 //glTranslatef(teapotX, teapotY, 0);
                 glTranslatef(-1.953333, +0.120001, 0);
-                glRotatef(angle, 0, 0, 1);
+                glRotatef(angle[3], 0, 0, 1);
                 glTranslatef(1.953333, -0.120001, 0);
                 if(ID==3) glColor3f(1,0,0);
                 else glColor3f(1,1,1);
@@ -75,19 +88,21 @@ int oldX = 0, oldY = 0;
 void motion(int x, int y){
     teapotX += (x - oldX)/150.0;
     teapotY -= (y - oldY)/150.0;
+    angle[ID] += (x-oldX);
     oldX = x;
     oldY = y;
-    angle = x;
     printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
     glutPostRedisplay();
 }
 void mouse(int button, int state, int x, int y)
 {
     if(state==GLUT_DOWN){
+        oldX = x;
+        oldY = y;
         teapotX = (x-150)/150.0;
         teapotY = (150-y)/150.0;
-        if(fout==NULL) fout = fopen("file4.txt","w");
-        fprintf(fout, "%f %f\n", teapotX, teapotY);
+        //if(fout==NULL) fout = fopen("file4.txt","w");
+        //fprintf(fout, "%f %f\n", teapotX, teapotY);
     }
     display();
 }
@@ -108,7 +123,7 @@ int main(int argc, char *argv[])//main()主函式 進階版
 
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);//雙緩衝區+3D深度功能
 
-    glutCreateWindow("week12");//開GLUT視窗
+    glutCreateWindow("week15");//開GLUT視窗
 
 
 
